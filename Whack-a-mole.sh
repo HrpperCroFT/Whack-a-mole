075d83a2a006d05a87486ab932a5e7cf9bd0a980
Vladimir <shestakov.vp@phystech.edu>
2023-04-20 00:30:30 +0300

    Added virtualenv launch

--- Whack-a-mole.sh
+++ Whack-a-mole.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-python -m pip install "kivy[base]" kivy_examples
+python3 -m pip install virtualenv
+python3 -m virtualenv venv
+source venv/bin/activate
+python3 -m pip install "kivy[base]" kivy_examples
 python3 main.py
